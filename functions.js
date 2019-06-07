1a56d4a701c4f501f07b1937e2099fd76bad1bcc
Briii <brianwkhuffman@gmail.com>
2019-06-07 01:08:33 -1000

    almost done

--- functions.js
+++ functions.js
@@ -3,21 +3,27 @@
  * @param {number} n
  * @return {string} the number as a string
  */
-
+function numberToString(n){
+    return n.toString();
+}
 
 /**
  * Adds one to a given number.
  * @param {number} n
  * @return {number}
  */
-
+function increase(n){
+    return n + 1;
+}
 
 /**
  * Subtracts one from a given number.
  * @param {number} n
  * @return {number}
  */
-
+function decrease(n){
+    return n - 1;
+}
 
 /**
  * Adds two numbers.
@@ -25,7 +31,9 @@
  * @param {number} y
  * @return {number} the sum
  */
-
+function add(x, y){
+    return x + y;
+}
 
 /**
  * Subtracts the second number from the first.
@@ -33,7 +41,9 @@
  * @param {number} y
  * @return {number} the difference
  */
-
+function subtract(x, y){
+    return x - y;
+}
 
 /**
  * Multiplies two numbers.
@@ -41,7 +51,9 @@
  * @param {number} y
  * @return {number} the product
  */
-
+function multiply(x, y){
+    return x * y;
+}
 
 /**
  * Divides the first number by the second.
@@ -49,14 +61,18 @@
  * @param {number} y
  * @return {number} the quotient
  */
-
+function divide(x, y){
+    return x/y;
+}
 
 /**
  * Multiplies a number by itself.
  * @param {number} x, number to be squared
  * @return {number} squared
  */
-
+function square(x){
+    return x * x;
+}
 
 /**
  * Performs a mathematical operation on two numbers.
@@ -66,7 +82,23 @@
  * @param {number} y
  * @return {number} the result
  */
-
+function calculate(operation, x, y){
+    if (operation === 'add'){
+        console.log(x + ' + ' + y + ' = ' + x+y);
+        return (x+y);
+    }else if (operation === 'subtract'){
+        console.log(x + ' - ' + y + ' = ' + x-y);
+        return (x-y);
+    }else if (operation === 'multiply'){
+        console.log(x + ' * ' + y + ' = ' + x*y);
+        return (x*y);
+    }else if (operation === 'divide'){
+        console.log(x + ' / ' + y + ' = ' + x/y);
+        return (x/y);
+    }else{
+        return 'does not compute';
+    }
+}
 
 /**
  * Returns true if `a` is greater than `b`.
@@ -74,7 +106,13 @@
  * @param {number} b
  * @return {boolean} `a` is larger than `b`
  */
-
+function isGreaterThan(a, b){
+    if (a>b){
+        return true;
+    }else{
+        return false;
+    }
+}
 
 /**
  * Returns true if `a` is less than `b`.
@@ -82,7 +120,13 @@
  * @param {number} b
  * @return {boolean} `a` is smaller than `b`
  */
-
+function isLessThan(a, b){
+    if (a<b){
+        return true;
+    }else{
+        return false;
+    }
+}
 
 /**
  * Returns true if `a` and `b` are equal.
@@ -90,7 +134,13 @@
  * @param {number} b
  * @return {boolean} the numbers are equal
  */
-
+function areEqual(a, b){
+    if (a===b){
+        return true;
+    }else{
+        return false;
+    }
+}
 
 /**
  * Returns the smallest value of two numbers.
@@ -98,7 +148,9 @@
  * @param {number} y
  * @return {number} the smallest number
  */
-
+function minimum(x, y){
+    return Math.min(x, y);
+}
 
 /**
  * Returns the largest value of two numbers.
@@ -106,21 +158,27 @@
  * @param {number} y
  * @return {number} the largest number
  */
-
+function maximum(x, y){
+    return Math.max(x, y);
+}
 
 /**
  * Returns true if `n` is even.
  * @param {number} n
  * @return {boolean} the number is even
  */
-
+function isEven(n){
+    return n%2==0;
+}
 
 /**
  * Returns true if `n` is odd.
  * @param {number} n
  * @return {boolean} the number is odd
  */
-
+function isOdd(n){
+    return n%2 !== 0;
+}
 
 /**
  * Returns a letter grade.
